bb55cc086f9f206bc0f0cf9735aad31f83d1b7eb
corybey <corybey0000@gmail.com>
2025-10-26 22:15:35 -0400

    Final

--- lib/widgets/category_card.dart
+++ lib/widgets/category_card.dart
@@ -1,2 +1,67 @@
 // Expense categories card widget
 // Used in home_screen.dart
+
+// category_card.dart
+import 'package:flutter/material.dart';
+
+class CategoryCard extends StatelessWidget {
+  final String name;
+  final String icon;
+  final VoidCallback? onTap;
+
+  const CategoryCard({
+    super.key,
+    required this.name,
+    required this.icon,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return Material(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(12),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(12),
+        onTap: onTap,
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.04),
+                blurRadius: 8,
+                offset: const Offset(0, 2),
+              ),
+            ],
+            border: Border.all(color: Colors.grey.withOpacity(0.15)),
+            color: Colors.white,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Emoji / icon
+              Text(icon, style: const TextStyle(fontSize: 28)),
+
+              const SizedBox(height: 8),
+
+              // Category name (caps/sentence case as you prefer)
+              Text(
+                name,
+                textAlign: TextAlign.center,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: textTheme.bodyMedium?.copyWith(
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
